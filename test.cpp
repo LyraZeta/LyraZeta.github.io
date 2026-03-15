f3a4dfef045786ae7dc569764dbd88629d6a6ef0
LyraZeta <lyrazeta@outlook.com>
2026-03-15 17:31:43 +0800

    Modify the front-end configuration

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    // int longestConsecutive(vector<int>& nums);
+    // nums=[1,0,1,2];
+    // sort(nums.begin(), nums.end());
+    // if(nums.size()==0) return 0;
+    // if(nums.size()==1) return 1;
+    // int ans=1, nown=1;
+    // for(auto it=nums.begin()+1; it!=nums.end();it++){
+    //     if(*it-1 == *(it-1)) nown++;
+    //     else {
+    //         ans = nown > ans?nown : ans;
+    //         nown = 1;
+    //     }
+    // }
+    // ans = nown > ans?nown : ans;
+    // return ans;
+    cout<<"N"<<endl<<"n";
+}
+
