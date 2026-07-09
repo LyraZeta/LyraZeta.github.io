331c7e4511b7c2a97940bb0663326857de3a773f
LyraZeta <lyrazeta@outlook.com>
2026-07-09 16:47:22 +0800

    Polish day mode layout and post navigation

--- js/main.js
+++ js/main.js
@@ -2,19 +2,10 @@ $(document).ready(function() {
 
 
   $('a.blog-button').click(function() {
-    // If already in blog, return early without animate overlay panel again.
     if (location.hash && location.hash == "#blog") return;
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
     $('.main-post-list').removeClass('hidden');
-    currentWidth = $('.panel-cover').width();
-    if (currentWidth < 2000) {
-      $('.panel-cover').addClass('panel-cover--collapsed');
-    } else {
-      $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '320px', 'width': '22%'}, 400, swing = 'swing', function() {} );
-    }
-
-    
+    $('.panel-cover').addClass('panel-cover--collapsed');
   });
 
   if (window.location.hash && window.location.hash == "#blog") {
